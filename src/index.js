59921f4d277b8e66ad7a9560fcfb83f7e6533007
Mohit Dayma <70746260+Mohit1310@users.noreply.github.com>
2023-02-24 05:04:35 +0530

    Adding props

--- src/index.js
+++ src/index.js
@@ -2,15 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Card from './Card';
-import 'tachyons'
 import reportWebVitals from './reportWebVitals';
+import 'tachyons'
+import { robots } from './robots';
+// as robots doesn't have an default export we use '{ robots }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Card />
-    <Card />
-    <Card />
+    <Card id = {robots[0].id} name = {robots[0].name} email = {robots[0].email}/>
+    <Card id = {robots[1].id} name = {robots[1].name} email = {robots[1].email}/>
+    <Card id = {robots[2].id} name = {robots[2].name} email = {robots[2].email}/>
   </React.StrictMode>
 );
 
