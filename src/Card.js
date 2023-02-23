e8c9e56a6baedad45113ff8c00196b0272a1b9b4
Mohit Dayma <70746260+Mohit1310@users.noreply.github.com>
2023-02-24 05:12:50 +0530

    destructuring props

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
-const Card = (props) => {
+// const Card = (props) => {
+//     const {name, email, id} = props // destructuring type-1
+const Card = ({ name, email, id}) => { // destructuring type-2
   return (
     <div className='tc bg-light-green dib br3 pa3 ma2 grow bw3 shadow-5'>
-        <img src={`https://robohash.org/${props.id}?200x200`} alt="robots" />
+        <img src={`https://robohash.org/${id}?200x200`} alt="robots" />
         <div>
-            <h2>{props.name}</h2>
-            <p>{props.email}</p>
+            <h2>{name}</h2>
+            <p>{email}</p>
         </div>
     </div>
   )
