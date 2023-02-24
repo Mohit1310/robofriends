7bcc0037f36552a2f642986eacb05f5cb4d1743c
Mohit Dayma <70746260+Mohit1310@users.noreply.github.com>
2023-02-24 05:39:14 +0530

    Adding father of all components i.e. App.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import CardList from './CardList';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons'
-import { robots } from './robots';
-// as robots doesn't have an default export we use '{ robots }
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CardList robots = {robots}/>
+    <App />
   </React.StrictMode>
 );
 
