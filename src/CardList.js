1480c20ced4341fc2a49e1851a9cb8520a233e9c
Mohit Dayma <70746260+Mohit1310@users.noreply.github.com>
2023-02-24 05:30:54 +0530

    Adding key prop

--- src/CardList.js
+++ src/CardList.js
@@ -2,12 +2,18 @@ import React from "react";
 import Card from "./Card";
 
 const CardList = ({ robots }) => {
-  const cardComponent = robots.map((user, i) => {
-    return <Card id={robots[i].id} name={robots[i].name} email={robots[i].email} />
-  })
   return (
     <div>
-      {cardComponent}
+      {robots.map((user, i) => {
+        return (
+          <Card
+            key={i} // This (Warning: Each child in a list should have a unique "key" prop) will come if don't use key if some card gets deleted react won't know which card is which so key prop is important.
+            id={robots[i].id}
+            name={robots[i].name}
+            email={robots[i].email}
+          />
+        );
+      })}
     </div>
   );
 };
