4a1d0e87459aee5e64ca1e1062def02a318a542a
Mohit Dayma <70746260+Mohit1310@users.noreply.github.com>
2023-04-06 16:05:12 +0530

    using hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,49 +1,39 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
 import "./App.css";
 
-class App extends Component {
-  // smart component
-  constructor() {
-    super();
-    this.state = {
-      robots: [],
-      searchfield: "",
-    };
-  }
+function App() {
+  const [robots, setRobots] = useState([])
+  const [searchfield, setSearchfield] = useState('');
 
-  componentDidMount() {
-    // getting response from api
+  useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((users) => this.setState({ robots: users }));
-  }
+      .then((users) => { setRobots(users) });
+  }, []) 
 
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value });
+  const onSearchChange = (event) => {
+    setSearchfield(event.target.value);
   };
 
-  render() {
-    const { robots, searchfield } = this.state;
-    const filteredRobots = robots.filter((robot) => {
-      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-    });
-    return !robots.length ? (
-      // if api response time is too much then this will be displayed
-      <h1 className="tc">Loading...</h1>
-    ) : (
-      <div className="tc">
-        <h1 className="f1">Robofriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <Scroll>
-          {/* CardList is now children of scroll */}
-          <CardList robots={filteredRobots} />
-        </Scroll>
-      </div>
-    );
-  }
+  const filteredRobots = robots.filter((robot) => {
+    return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+  });
+  return !robots.length ? (
+    // if api response time is too much then this will be displayed
+    <h1 className="tc">Loading...</h1>
+  ) : (
+    <div className="tc">
+      <h1 className="f1">Robofriends</h1>
+      <SearchBox searchChange={onSearchChange} />
+      <Scroll>
+        {/* CardList is now children of scroll */}
+        <CardList robots={filteredRobots} />
+      </Scroll>
+    </div>
+  );
 }
 
 export default App;
